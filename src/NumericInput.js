e1643cc97552e6e99f0fb68813c3453059718e7b
surxbhi <47130821+surxbhi@users.noreply.github.com>
2020-01-02 16:12:08 -0700

    initial commit

---
+++ src/NumericInput.js
@@ -0,0 +1,62 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import {
+    Input,
+    Tooltip,
+  } from "antd";
+
+function formatNumber(value) {
+  value += "";
+  const list = value.split(".");
+  const prefix = list[0].charAt(0) === "-" ? "-" : "";
+  let num = prefix ? list[0].slice(1) : list[0];
+  let result = "";
+  while (num.length > 3) {
+    result = `,${num.slice(-3)}${result}`;
+    num = num.slice(0, num.length - 3);
+  }
+  if (num) {
+    result = num + result;
+  }
+  return `${prefix}${result}${list[1] ? `.${list[1]}` : ""}`;
+}
+
+export default class NumericInput extends React.Component {
+  onChange = e => {
+    const { value } = e.target;
+    const reg = /^-?(0|[1-9][0-9]*)(\.[0-9]*)?$/;
+    if ((!isNaN(value) && reg.test(value)) || value === "" || value === "-") {
+      this.props.onChange(value);
+    }
+  };
+
+  // '.' at the end or only '-' in the input box.
+  onBlur = () => {
+    const { value, onBlur, onChange } = this.props;
+    if (value.charAt(value.length - 1) === "." || value === "-") {
+      onChange(value.slice(0, -1));
+    }
+    if (onBlur) {
+      onBlur();
+    }
+  };
+  render() {
+    const { value } = this.props;
+    return (
+      <Tooltip
+        trigger={["focus"]}
+        placement="topLeft"
+        overlayClassName="numeric-input"
+      >
+        <Input
+          {...this.props}
+          onChange={this.onChange}
+          onBlur={this.onBlur}
+          maxLength={25}
+        />
+      </Tooltip>
+    );
+  }
+}
---
+++ src/Security.js
@@ -0,0 +1,45 @@
+
+import React, { Component } from 'react';
+import { ReCaptcha } from 'react-recaptcha-google'
+
+class Security extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.onLoadRecaptcha = this.onLoadRecaptcha.bind(this);
+    this.verifyCallback = this.verifyCallback.bind(this);
+  }
+  componentDidMount() {
+    if (this.captchaDemo) {
+        console.log("started, just a second...")
+        this.captchaDemo.reset();
+    }
+    
+  }
+  onLoadRecaptcha() {
+      if (this.captchaDemo) {
+          this.captchaDemo.reset();
+      }
+  }
+  verifyCallback(recaptchaToken) {
+    // Here you will get the final recaptchaToken!!!  
+    console.log(recaptchaToken, "<= your recaptcha token")
+  }
+  render() {
+    return (
+      <div>
+        {/* You can replace captchaDemo with any ref word */}
+        <ReCaptcha
+            ref={(el) => {this.captchaDemo = el;}}
+            size="normal"
+            data-theme="dark"            
+            render="explicit"
+            sitekey="6Le62ssUAAAAAAXA_s2GKUjofjBywzdtqp5bF71j"
+            onloadCallback={this.onLoadRecaptcha}
+            verifyCallback={this.verifyCallback}
+        />
+      </div>
+    );
+  };
+};
+export default Security;
+
